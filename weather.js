052e37b74d9f2887407c7a1a3a7bf5f1345fccec
Christian Sanchez <cas81695@gmail.com>
2020-02-05 23:39:22 -0500

    added function for displaying previous searches

--- weather.js
+++ weather.js
@@ -30,8 +30,28 @@ function success(position) {
         currentCity = response.name;
         saveLoc = response.name;
         getCurrent(currentCity);
-    }
+    });
 
 
 }
 
+function error(){
+    currentCity = "Cary"
+    getCurrent(currentCity);
+}
+
+function displayPrev() {
+    if (previousCities) {
+        $("#previousSearches").empty();
+        var buttons = $("<div>").attr("class", "list-group");
+        for (var i = 0; i < previousCities.length; i++) {
+            var cityButton = $("<a>").attr("href", "#").attr("id", "city-button").text(previousCities[i]);
+            if (previousCities[i]) == currentCity){
+                cityButton.attr("class", "list-group-item list-group-item-action active");
+
+            } else {
+                cityButton.attr("class", "list-group-item list-group-item-action");
+            } buttons.prepend(cityButton);
+        } $("previousSearches").append(buttons);
+    }
+}
